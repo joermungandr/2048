59537d0fad7d1641cb30cc8a843dde97ea730a5a
Steven Stadler <steven.stadler@gmail.com>
2014-12-25 20:46:19 +0100

    Adds basic tests

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,29 +1,5 @@
 'use strict';
 
-/**
- * @ngdoc overview
- * @name twentyfourtyeightApp
- * @description
- * # twentyfourtyeightApp
- *
- * Main module of the application.
- */
-angular
-  .module('twentyfourtyeightApp', ['Game'])
-  .controller('GameController', function(GameManager) {
-    this.game = GameManager;
-  });
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+angular.module('twentyfourtyeightApp', ['Game']).controller('GameController', function(GameManager) {
+  this.game = GameManager;
+});
